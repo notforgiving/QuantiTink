f8cba9daccc85a8d2d8907a0074bc9be3e523e49
notforgiving <notforgiving@list.ru>
2025-07-10 20:36:49 +0300

    new design

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -3,9 +3,13 @@ import css from "./styles.module.scss";
 import Button from "UI/components/Button";
 import { useAuth } from "hooks/useAuth";
 import { ReactComponent as EmailSvg } from "assets/email.svg";
+import { useDispatch } from "react-redux";
+import { tokenSlice } from "store/slices/token.slice";
+import { userSlice } from "store/slices/user.slice";
 
 const Profile: FC = () => {
   const { email } = useAuth();
+  const dispatch = useDispatch();
   return (
     <div className={css.profile}>
       <div className={css.profile__header}>Ваши данные</div>
@@ -17,7 +21,16 @@ const Profile: FC = () => {
           <div className={css.profile__gridData}>{email}</div>
         </div>
       </div>
-      <Button text="Выйти" className={css.profile__exit} />
+      <Button
+        text="Выйти"
+        className={css.profile__exit}
+        buttonAttributes={{
+          onClick: () => {
+            dispatch(userSlice.actions.removeUserAction());
+            dispatch(tokenSlice.actions.removeTokenAction());
+          },
+        }}
+      />
     </div>
   );
 };
