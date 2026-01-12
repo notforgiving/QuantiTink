d26e10ffcad0cf34d188f09ada2eb017689b3541
notforgiving <notforgiving@list.ru>
2026-01-12 13:20:49 +0300

    fixed colors for sector view

--- src/Pages/SharesPage/index.tsx
+++ src/Pages/SharesPage/index.tsx
@@ -34,26 +34,30 @@ const SharesPage: FC = () => {
         {tab === "BASE" && !shares?.length && <div>В портфеле нет акций</div>}
         {tab === "BASE" &&
           shares &&
-          shares.map((el, index) => (
-            <div
-              className={css.shares__item}
-              key={el.figi}
-              onClick={() => navigate(`/${id}/shares/${el.figi}`)}
-            >
+          shares.map((el, index) => {
+            if (!el.brand.logoBaseColor || !el.brand.logoName) return null;
+
+            return (
               <div
-                className={css.shares__item_icon}
-                style={{
-                  backgroundColor: `${el.brand.logoBaseColor}`,
-                  backgroundImage: `url(https://invest-brands.cdn-tinkoff.ru/${el.brand.logoName.replace(
-                    ".png",
-                    ""
-                  )}x160.png)`,
-                }}
-              />
-              <div className={css.shares__item_name}>{el.name}</div>
-              <div className={css.shares__item_precent}>{el.percent} %</div>
-            </div>
-          ))}
+                className={css.shares__item}
+                key={el.figi}
+                onClick={() => navigate(`/${id}/shares/${el.figi}`)}
+              >
+                <div
+                  className={css.shares__item_icon}
+                  style={{
+                    backgroundColor: `${el.brand.logoBaseColor}`,
+                    backgroundImage: `url(https://invest-brands.cdn-tinkoff.ru/${el.brand.logoName.replace(
+                      ".png",
+                      ""
+                    )}x160.png)`,
+                  }}
+                />
+                <div className={css.shares__item_name}>{el.name}</div>
+                <div className={css.shares__item_precent}>{el.percent} %</div>
+              </div>
+            );
+          })}
         {tab === "SECTOR" && (
           <>
             {sectors.length === 0 && <div>Нет акций по секторам</div>}
