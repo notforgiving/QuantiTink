2e8629e9004f9fe538e6f78d3879df92504ac11f
notforgiving <notforgiving@list.ru>
2026-07-21 10:58:15 +0300

    add test tbank api

--- api/test-tbank.js
+++ api/test-tbank.js
@@ -1,9 +1,5 @@
-export default async function handler(req: any, res: any) {
+module.exports = async function handler(req, res) {
   try {
-    console.log("Function started");
-
-    console.log("Token exists:", !!process.env.TBANK_TOKEN);
-
     const response = await fetch(
       "https://invest-public-api.tbank.ru/rest/tinkoff.public.invest.api.contract.v1.UsersService/GetAccounts",
       {
@@ -18,20 +14,14 @@ export default async function handler(req: any, res: any) {
 
     const text = await response.text();
 
-    console.log("Status:", response.status);
-
     return res.status(200).json({
       status: response.status,
       body: text,
     });
-  } catch (e: any) {
-    console.error("ERROR:", e);
-    console.error("CAUSE:", e?.cause);
-
+  } catch (e) {
     return res.status(500).json({
-      message: e?.message,
-      cause: e?.cause,
-      stack: e?.stack,
+      message: e.message,
+      cause: e.cause,
     });
   }
-}
+};
